61240d4ab4726236d96899e5d03d1e6b867467b9
kvmduc <kvmduc3@gmail.com>
2020-04-30 01:52:16 +0700

    Complete Ampersand

--- Project1/shell.c
+++ Project1/shell.c
@@ -2,11 +2,13 @@
 
 int main(void){
     char input_string[MAX_LINE] = {};/* command line string */
+    char input_string_temp[MAX_LINE] = {};
     char * args_normal[MAX_LIST] = {};
     char * args_pipe[MAX_LIST] = {};
     int shouldrun = 1; /* flag to determine when to exit program */
     int system_Num;
     do{
+        int background = 0;
         printf("osh");
         fflush(stdout);
         /**
@@ -17,18 +19,25 @@ int main(void){
         if(take_input(input_string) == 0){
            continue;
         }
-       
+
+        if(strstr(input_string,"&") != NULL){ // Tim kiem Ampersand
+            strncpy(input_string_temp,input_string, strlen(input_string) - 1);
+            strcpy(input_string,input_string_temp);
+            background = 1;
+        }
+
         system_Num = input_classification(input_string,args_normal,args_pipe);
+
         if(system_Num == -1){
             break;
         }
         if(system_Num == 0){
-            shouldrun = osh_normal_execute(args_normal);
+            shouldrun = osh_normal_execute(args_normal, background);
         }
-        if(system_Num == 1){
+        else if(system_Num == 1){
             //
         }
-        if(system_Num == 2){
+        else if(system_Num == 2){
             continue;
         }
     } while(shouldrun);
